bb57af728f48fbfc89d5b756e5d7cbc542e16a6f
mariamy <raphava.mari@gmail.com>
2022-04-28 17:48:16 +0400

    tring to publish timer

--- src/entry.esm.ts
+++ src/entry.esm.ts
@@ -1,7 +1,7 @@
 import { App, Plugin } from 'vue';
 
 // Import vue component
-import component from '@/vue3-otp.vue';
+import * as component from '@/vue3-otp.vue';
 
 // Define typescript interfaces for installable component
 type InstallableComponent = typeof component & { install: Exclude<Plugin['install'], undefined> };
